cce234a44380cccab5283758a541489895b26794
a21mariopm <a21mariopm@a13eq15.sanclemente.local>
2022-02-18 16:47:21 +0100

    Creamos la clase ContadorAscendente()

---
+++ ProxectoContadorAscendente/src/main/java/com/mycompany/proxectocontadorascendente/ContadorAscendente.java
@@ -0,0 +1,14 @@
+package com.mycompany.proxectocontadorascendente;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author a21mariopm
+ */
+public class ContadorAscendente {
+    
+}
