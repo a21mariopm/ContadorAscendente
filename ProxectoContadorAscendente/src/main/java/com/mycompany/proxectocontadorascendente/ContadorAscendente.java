08ed67fa0adef75a46cb3c4aa4b67f7a5b646a2e
a21mariopm <a21mariopm@a13eq15.sanclemente.local>
2022-02-18 16:58:39 +0100

    Creamos la clase ContadorAscendente()

--- ProxectoContadorAscendente/src/main/java/com/mycompany/proxectocontadorascendente/ContadorAscendente.java
+++ ProxectoContadorAscendente/src/main/java/com/mycompany/proxectocontadorascendente/ContadorAscendente.java
@@ -10,5 +10,37 @@ package com.mycompany.proxectocontadorascendente;
  * @author a21mariopm
  */
 public class ContadorAscendente {
+  
+    private int valorActual;
+
+    public ContadorAscendente(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    private
     
+    /**
+     * Get the value of valorActual
+     *
+     * @return the value of valorActual
+     */
+    public int getValorActual() {
+        return valorActual;
+    }
+
+    /**
+     * Set the value of valorActual
+     *
+     * @param valorActual new value of valorActual
+     */
+    public void setValorActual(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    @Override
+    public String toString() {
+        return "ContadorAscendente{" + "valorActual=" + valorActual + '}';
+    }
+  
+  }
 }
