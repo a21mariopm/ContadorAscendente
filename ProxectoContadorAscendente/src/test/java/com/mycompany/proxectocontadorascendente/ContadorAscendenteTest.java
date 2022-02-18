cdb541b0105a292a8a1b9a90bf1a49e97a366a37
a21mariopm <a21mariopm@a13eq15.sanclemente.local>
2022-02-18 17:18:14 +0100

    Actualizamos pom.xml

---
+++ ProxectoContadorAscendente/src/test/java/com/mycompany/proxectocontadorascendente/ContadorAscendenteTest.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.proxectocontadorascendente;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author a21mariopm
+ */
+public class ContadorAscendenteTest {
+    
+    public ContadorAscendenteTest() {
+    }
+
+    /**
+     * Test of incrementar method, of class ContadorAscendente.
+     */
+    @Test
+    public void testIncrementar() {
+        System.out.println("incrementar");
+        ContadorAscendente instance = null;
+        int expResult = 0;
+        int result = instance.incrementar();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of reiniciar method, of class ContadorAscendente.
+     */
+    @Test
+    public void testReiniciar() {
+        System.out.println("reiniciar");
+        ContadorAscendente instance = null;
+        instance.reiniciar();
+    }
+
+    /**
+     * Test of cambiarValor method, of class ContadorAscendente.
+     */
+    @Test
+    public void testCambiarValor() {
+        System.out.println("cambiarValor");
+        int modificador = 0;
+        ContadorAscendente instance = null;
+        int expResult = 0;
+        int result = instance.cambiarValor(modificador);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getValorActual method, of class ContadorAscendente.
+     */
+    @Test
+    public void testGetValorActual() {
+        System.out.println("getValorActual");
+        ContadorAscendente instance = null;
+        int expResult = 0;
+        int result = instance.getValorActual();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setValorActual method, of class ContadorAscendente.
+     */
+    @Test
+    public void testSetValorActual() {
+        System.out.println("setValorActual");
+        int valorActual = 0;
+        ContadorAscendente instance = null;
+        instance.setValorActual(valorActual);
+    }
+
+    /**
+     * Test of toString method, of class ContadorAscendente.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        ContadorAscendente instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+    
+}
